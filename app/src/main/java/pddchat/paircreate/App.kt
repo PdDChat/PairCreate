023211ec12e9fc2c356b0642a90bbba66022e1f0
tateno <tateno-masa@gmail.com>
2020-04-21 13:12:47 +0900

    Contextをどこからでも参照できるようにApplicationクラスに定義

--- app/src/main/java/pddchat/paircreate/App.kt
+++ app/src/main/java/pddchat/paircreate/App.kt
@@ -1,15 +1,22 @@
 package pddchat.paircreate
 
 import android.app.Application
+import android.content.Context
 import pddchat.paircreate.log.AppTree
 import timber.log.Timber
 
 class App : Application() {
 
+    companion object {
+        // TODO どこからでも呼べるように一旦定義。DaggerでDIするように修正する
+        lateinit var context: Context
+    }
+
     override fun onCreate() {
         super.onCreate()
-
         setupLogger()
+
+        context = this
     }
 
     private fun setupLogger() {
