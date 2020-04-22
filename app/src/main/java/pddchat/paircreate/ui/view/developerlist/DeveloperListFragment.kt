8f52ef5b809a61b652f7ddbf8c285ec124ac4a41
tateno <tateno-masa@gmail.com>
2020-04-22 16:22:10 +0900

    開発者リスト画面のButton、EditTextをDataBindingに変更

--- app/src/main/java/pddchat/paircreate/ui/view/developerlist/DeveloperListFragment.kt
+++ app/src/main/java/pddchat/paircreate/ui/view/developerlist/DeveloperListFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import pddchat.paircreate.R
 import pddchat.paircreate.databinding.FragmentDeveloperListBinding
 import pddchat.paircreate.ui.viewmodel.DeveloperListViewModel
 
@@ -38,41 +34,6 @@ class DeveloperListFragment : Fragment() {
         viewModel.observeDeveloper()
         viewModel.developerList.observe(viewLifecycleOwner) {
             adapter.submitList(it)
-        }
-
-        var registerName = ""
-        val editText = view.findViewById<EditText>(R.id.edit_developer_name)
-        editText.setOnClickListener {
-            // TODO キーボード制御
-            if (editText.text != null) {
-                registerName = editText.text.toString()
-            }
-        }
-
-        val registerButton = view.findViewById<Button>(R.id.register_button)
-        registerButton.setOnClickListener {
-            if (registerName.isEmpty()) {
-                Toast.makeText(context, "登録する名前を入力してください", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            viewModel.register(registerName)
-
-            // 登録名の初期化
-            registerName = ""
-            editText.text.clear()
-        }
-
-        val deleteButton = view.findViewById<Button>(R.id.delete_button)
-        deleteButton.setOnClickListener {
-            if (registerName.isEmpty()) {
-                Toast.makeText(context, "削除する名前を入力してください", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            viewModel.delete(registerName)
-
-            // 削除名の初期化
-            registerName = ""
-            editText.text.clear()
         }
 
         fetchDeveloperList(viewModel, binding.developerRecyclerView)
--- app/src/main/java/pddchat/paircreate/ui/viewmodel/DeveloperListViewModel.kt
+++ app/src/main/java/pddchat/paircreate/ui/viewmodel/DeveloperListViewModel.kt
@@ -1,29 +1,47 @@
 package pddchat.paircreate.ui.viewmodel
 
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import pddchat.paircreate.App.Companion.context
 import pddchat.paircreate.data.model.Developer
 import pddchat.paircreate.data.repository.DeveloperListRepository
 
 class DeveloperListViewModel : ViewModel() {
+
+    val name = MutableLiveData<String>()
 
     private val _developerList: MutableLiveData<List<Developer>> = MutableLiveData()
     val developerList: LiveData<List<Developer>> = _developerList
 
     private val repository: DeveloperListRepository = DeveloperListRepository()
 
-    fun register(newName: String) {
+    fun observeDeveloper() {
+        _developerList.value = repository.getDeveloperlist()
+    }
+
+    fun register() {
         val devList = _developerList.value
+        val newName = name.value ?: ""
+
+        if(newName.isEmpty()) {
+            Toast.makeText(context, "登録する名前を入力してください", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         _developerList.value = repository.update(newName, devList)
     }
 
-    fun delete(deleteName: String) {
+    fun delete() {
         val devList = _developerList.value
-        _developerList.value = repository.delete(deleteName, devList)
-    }
+        val deleteName = name.value ?: ""
 
-    fun observeDeveloper() {
-        _developerList.value = repository.getDeveloperlist()
+        if(deleteName.isEmpty()) {
+            Toast.makeText(context, "削除する名前を入力してください", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        _developerList.value = repository.delete(deleteName, devList)
     }
 }
