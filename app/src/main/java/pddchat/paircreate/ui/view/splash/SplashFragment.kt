2161412cf0205268a3259b5466f773e57e67aa3f
tateno <tateno-masa@gmail.com>
2020-04-24 11:49:56 +0900

    スプラッシュ画面をDataBindingを利用するように変更

--- app/src/main/java/pddchat/paircreate/ui/view/splash/SplashFragment.kt
+++ app/src/main/java/pddchat/paircreate/ui/view/splash/SplashFragment.kt
@@ -7,20 +7,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import pddchat.paircreate.R
+import pddchat.paircreate.databinding.FragmentSplashBinding
+import pddchat.paircreate.ui.viewmodel.SplashViewModel
 
 class SplashFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navController = this.findNavController()
-        navController.navigate(R.id.splash_to_pair_list)
+
+        val viewModel = SplashViewModel()
+        binding.viewModel = viewModel
+
+        findNavController().navigate(R.id.splash_to_pair_list)
     }
 }
---
+++ app/src/main/java/pddchat/paircreate/ui/viewmodel/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package pddchat.paircreate.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    val splashTitle = "splash"
+}
